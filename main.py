6f81b6dd7ae7a1e4ad0b77a820006a5be486a9dc
Alex Müller <alexarnimueller@gmail.com>
2020-09-22 15:16:09 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,58 @@
+
+import concurrent.futures
+
+import flask
+from google.cloud import bigquery
+
+
+app = flask.Flask(__name__)
+bigquery_client = bigquery.Client()
+
+
+@app.route("/")
+def main():
+    query_job = bigquery_client.query(
+        """
+        SELECT map.summary_polyline 
+        FROM `activities.alex`
+        """
+    )
+
+    return flask.redirect(
+        flask.url_for(
+            "results",
+            project_id=query_job.project,
+            job_id=query_job.job_id,
+            location=query_job.location,
+        )
+    )
+
+
+@app.route("/results")
+def results():
+    project_id = flask.request.args.get("project_id")
+    job_id = flask.request.args.get("job_id")
+    location = flask.request.args.get("location")
+
+    query_job = bigquery_client.get_job(
+        job_id,
+        project=project_id,
+        location=location,
+    )
+
+    try:
+        # Set a timeout because queries could take longer than one minute.
+        results = query_job.result(timeout=30)
+        results = [r.replace('\\', '\\\\') for r in results]
+    except concurrent.futures.TimeoutError:
+        return flask.render_template("timeout.html", job_id=query_job.job_id)
+
+    return flask.render_template("index.html", routes=results)
+
+
+if __name__ == "__main__":
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    app.run(host="127.0.0.1", port=8080, debug=True)
+# [END gae_python38_bigquery]
